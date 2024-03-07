2b4ece6600026ce0a2c1994b7f562c60806df8a7
Valentin Malassigne-Lepicard <vmalassi@c1r6p2.42lehavre.fr>
2024-03-07 19:26:11 +0100

    fix before rendu

--- cpp/cpp01/ex02/main.cpp
+++ cpp/cpp01/ex02/main.cpp
@@ -4,13 +4,15 @@ int main()
 {
 	std::string hi_string = "HI THIS IS BRAIN";
 	std::string* stringPTR = &hi_string;
-	std::string& ref_to_str = hi_string;
+	std::string& stringREF = hi_string;
 
 	std::cout << "address of hi_string : " << &hi_string << std::endl;
 	std::cout << "address held by stringPTR : " << stringPTR << std::endl;
-	std::cout << "address held by ref_to_str : " << &ref_to_str << std::endl;
+	std::cout << "address held by ref_to_str : " << &stringREF << std::endl;
 
 	std::cout << "value of hi_string : " << hi_string << std::endl;
 	std::cout << "value of pointed by stringPTR : " << *stringPTR << std::endl;
-	std::cout << "value of ref_to_str : " << ref_to_str << std::endl;
+	std::cout << "value of ref_to_str : " << stringREF << std::endl;
+
+	return (0);
 }

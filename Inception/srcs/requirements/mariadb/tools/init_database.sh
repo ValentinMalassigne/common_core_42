885db8ff41662bc17184087ce884604fe24fbab3
ValentinMalassigne <valentin.malassigne3@gmail.com>
2024-09-03 11:54:02 +0200

    fix syntax and better make file

--- Inception/srcs/requirements/mariadb/tools/init_database.sh
+++ Inception/srcs/requirements/mariadb/tools/init_database.sh
@@ -6,7 +6,7 @@ mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '$
 
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`{$SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL8ROOT8PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 mysql -e "FLUSH PRIVILEGES;"
 

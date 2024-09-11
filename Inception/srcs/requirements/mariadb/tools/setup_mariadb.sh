5113cf04767a7fcd3a7a2534c44214fa5612ce6b
Valentin Malassigne-Lepicard <vmalassi@c2r2p1.42lehavre.fr>
2024-09-11 13:05:55 +0200

    rendu ?

--- Inception/srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -23,7 +23,4 @@ then
 	rm -f ${TMP}
 fi
 
-# sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-
 exec /usr/bin/mysqld --user=mysql --console
--- Inception/srcs/requirements/wordpress/tools/setup-wordpress.sh
+++ Inception/srcs/requirements/wordpress/tools/setup-wordpress.sh
@@ -6,11 +6,10 @@ done
 if [ ! -f ${WP_PATH}/wp-config.php ]
 then
 	wp core download --allow-root
-	wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASSWORD} --dbhost=${DB_HOST} --path=${WP_PATH} --allow-root
-	wp core install --url=${HOSTNAME}/wordpress --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --path=${WP_PATH} --allow-root
-	wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=subscriber --display_name=${WP_USER} --porcelain --path=${WP_PATH} --allow-root
-	wp theme install blocksy --path=${WP_PATH} --activate --allow-root
-	wp theme status blocksy --allow-root
+	wp config create --allow-root --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASSWORD} --dbhost=${DB_HOST} --path=${WP_PATH}
+	wp core install --allow-root --url=${HOSTNAME}/wordpress --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --path=${WP_PATH}
+	wp user create --allow-root ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=subscriber --display_name=${WP_USER} --porcelain --path=${WP_PATH}
+	wp theme install blocksy --allow-root --path=${WP_PATH} --activate
 fi
 
 exec /usr/sbin/php-fpm81 -F -R
